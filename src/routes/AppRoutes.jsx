2767e7f4e01283b335ad30137dd1bf6533e9c697
HussienSayed14 <hussiens399@gmail.com>
2024-11-23 00:28:51 +0200

    Adding Initial layout

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
