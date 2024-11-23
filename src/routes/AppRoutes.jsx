92a77144be73f02551c254c630317e98f31a5a85
HussienSayed14 <hussiens399@gmail.com>
2024-11-23 17:23:56 +0200

    Migrating to Tailwinf css

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Test from "../components/Test";
 const AppRoutes = () => {
   return (
     <Router>
-      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+      <Routes>
+        <Route path="/" element={<Test />} />
+      </Routes>
     </Router>
   );
 };
