4f199d3af857d172dcaf2c2ac2918be104397619
HussienSayed14 <hussiens399@gmail.com>
2024-12-14 21:08:58 +0200

    Making NavBar Sticky

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -53,7 +53,10 @@ export default function NavBar() {
   };
 
   return (
-    <Disclosure as="nav" className="bg-gray-800 dark:bg-gray-900">
+    <Disclosure
+      as="nav"
+      className="bg-gray-800 dark:bg-gray-900 shadow-md sticky top-0 z-50"
+    >
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
